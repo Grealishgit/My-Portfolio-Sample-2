46dc417600930e254e6a27fbdbdca5516fbbe3eb
Hunter <eugyneehunter@gmail.com>
2025-04-16 12:35:38 +0300

    Modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaInstagram, FaXTwitter, FaLinkedin, FaGithub } from "react-icons/fa6";
+import image from '../assets/netflix5.jpg'
 
 const Footer = () => {
   return (
-    <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+    <footer className="w-full py-4 bg-black-100/55 text-white flex md:flex-row flex-col justify-center gap-6">
+      <div className='md:flex hidden justify-center'>
+        <p>© 2025 @ <span className="text-amber-500  font-bold">HunterDev</span>. All rights Reserved</p>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-6">
       <a
-        href="https://www.instagram.com/ahmedz/"
+          href="https://www.instagram.com/wizardhunter1/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/Eugene" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.comhttps://x.com/hunter_20" target="_blank" rel="noopener noreferrer">
         <FaXTwitter size={24} />
       </a>
       <a
@@ -20,7 +25,19 @@ const Footer = () => {
         rel="noopener noreferrer"
       >
         <FaLinkedin size={24} />
-      </a>
+        </a>
+        <a
+          href="https://github.com/Grealishgit"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={24} />
+        </a>
+      </div>
+      <img src={image} className="h-7 md:block hidden w-7 rounded-full aspect-square cursor-pointer" alt="" />
+      <div className='flex md:hidden justify-center'>
+        <p>© 2025 @ <span className="text-amber-500  font-bold">HunterDev</span>. All rights Reserved</p>
+      </div>
     </footer>
   );
 };
